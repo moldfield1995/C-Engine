030c19ffd5a36be213fe99d37e1f8b9daa7e7dea
Matthew Oldfield <moldfield95@googlemail.com>
2018-04-17 13:56:59 +0100

    started on slider shader

---
+++ Engine/slidershaderclass.h
@@ -0,0 +1,64 @@
+
+
+//////////////
+// INCLUDES //
+//////////////
+#include <d3d11.h>
+#include <d3dcompiler.h>
+#include <directxmath.h>
+#include <fstream>
+#include "Shader.h"
+using namespace DirectX;
+using namespace std;
+
+//Created By RasterTeck
+//Updated By Matthew Oldfield
+
+////////////////////////////////////////////////////////////////////////////////
+// Class name: SliderShaderClass
+////////////////////////////////////////////////////////////////////////////////
+class SliderShaderClass:public Shader
+{
+private:
+	struct MatrixBufferType
+	{
+		XMMATRIX world;
+		XMMATRIX view;
+		XMMATRIX projection;
+	};
+
+	struct ColourBuffer
+	{
+		XMFLOAT4 colour;
+		XMFLOAT2 UVClip;
+	};
+
+public:
+	SliderShaderClass();
+	SliderShaderClass(const SliderShaderClass&);
+	~SliderShaderClass();
+
+	bool Initialize(ID3D11Device*, HWND);
+	void Shutdown();
+	//New
+	bool Render(ID3D11DeviceContext * context, int indexCount, const XMMATRIX &worldMatrix, const XMMATRIX &viewMatrix, const XMMATRIX &projectionMatrix, std::vector< ID3D11ShaderResourceView*>* Sliders, LightClass * light = 0, XMFLOAT4* colour = 0);
+	//Old
+	bool Render(ID3D11DeviceContext*, int, const XMMATRIX&, const XMMATRIX&, const XMMATRIX&, ID3D11ShaderResourceView*);
+
+private:
+	bool InitializeShader(ID3D11Device*, HWND, WCHAR*, WCHAR*);
+	void ShutdownShader();
+	void OutputShaderErrorMessage(ID3D10Blob*, HWND, WCHAR*);
+
+	bool SetShaderParameters(ID3D11DeviceContext*, const XMMATRIX&, const XMMATRIX&, const XMMATRIX&, ID3D11ShaderResourceView*, XMFLOAT4);
+	void RenderShader(ID3D11DeviceContext*, int);
+
+private:
+	ID3D11VertexShader* m_vertexShader;
+	ID3D11PixelShader* m_pixelShader;
+	ID3D11InputLayout* m_layout;
+	ID3D11Buffer* m_matrixBuffer;
+	ID3D11SamplerState* m_sampleState;
+
+	ID3D11Buffer* m_sliderBuffer;
+};
